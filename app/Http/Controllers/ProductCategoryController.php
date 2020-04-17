5a63eadd6aa4b22203f91532f4e9f3350a5c623d
Loic <loic>
2020-04-17 15:11:12 +0200

    add create and update for product categories

--- app/Http/Controllers/ProductCategoryController.php
+++ app/Http/Controllers/ProductCategoryController.php
@@ -17,4 +17,27 @@ class ProductCategoryController extends Controller
         $products = $category->products()->get();
         return response()->json([$category, $products], 200);
     }
+    public function create(Request $request)
+    {
+        $this->validate($request, ProductCategory::getRules());
+        $category = new ProductCategory($request->toArray());
+        $category->save();
+        return response()->json($category,200);
+    }
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, ProductCategory::getRules());
+        $category = ProductCategory::find($id);
+        if($category == null)
+        {
+            return response()->json('The product category doesn\'t exist.', 404);
+        }
+        else
+        {
+            $category->fill($request->all());
+            $category->save();
+            return response()->json($category,200);
+        }
+
+    }
 }
